4c170f2e47de57377f4161ece79cc40126b20430
zwelisha <zwescientist@gmail.com>
2023-06-12 09:15:20 +0200

    Completed registration page

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home, register
+from .views import home, register, login
 urlpatterns = [
    path('home/', home, name="home-page"),
-   path('register/', register, name="register-page")
+   path('register/', register, name="register-page"),
+   path('login/', login, name="login-page")
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User 
 
 # Create your views here.
 
@@ -6,7 +7,20 @@ def home(request):
     return render(request, 'auth_app/index.html', {})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('userName')
+        first_name = request.POST.get('userFirstName')
+        last_name = request.POST.get('userLastName')
+        email = request.POST.get('userEmail')
+        password = request.POST.get('userPassword')
+
+        user = User.objects.create_user(username,email,password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return redirect('login-page')
+
     return render(request, 'auth_app/register.html', {})
 
 def login(request):
-    pass
+    return render(request, 'auth_app/login.html', {})

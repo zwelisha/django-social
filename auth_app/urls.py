0a7eb95c0ae6c7b9c6c8d95862169e3a50fde627
zwelisha <zwescientist@gmail.com>
2023-06-12 12:52:58 +0200

    Formattd code with black

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import home, register, user_login, user_logout
+
 urlpatterns = [
-   path('home/', home, name="home-page"),
-   path('register/', register, name="register-page"),
-   path('login/', user_login, name="login-page"),
-   path('logout/', user_logout, name="logout")
+    path("home/", home, name="home-page"),
+    path("register/", register, name="register-page"),
+    path("login/", user_login, name="login-page"),
+    path("logout/", user_logout, name="logout"),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,41 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
+
 
 @login_required
 def home(request):
-    return render(request, 'auth_app/index.html', {})
+    return render(request, "auth_app/index.html", {})
+
 
 def register(request):
-    if request.method == 'POST':
-        username = request.POST.get('userName')
-        first_name = request.POST.get('userFirstName')
-        last_name = request.POST.get('userLastName')
-        email = request.POST.get('userEmail')
-        password = request.POST.get('userPassword')
+    if request.method == "POST":
+        username = request.POST.get("userName")
+        first_name = request.POST.get("userFirstName")
+        last_name = request.POST.get("userLastName")
+        email = request.POST.get("userEmail")
+        password = request.POST.get("userPassword")
 
-        user = User.objects.create_user(username,email,password)
+        user = User.objects.create_user(username, email, password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
-        return redirect('login-page')
+        return redirect("login-page")
 
-    return render(request, 'auth_app/register.html', {})
+    return render(request, "auth_app/register.html", {})
+
 
 def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['userName']
-        password = request.POST['userPassword']
+    if request.method == "POST":
+        username = request.POST["userName"]
+        password = request.POST["userPassword"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home-page')
+            return redirect("home-page")
         else:
             return HttpResponse("User not found!")
-    return render(request, 'auth_app/login.html', {})
+    return render(request, "auth_app/login.html", {})
+
 
 def user_logout(request):
     logout(request)
-    return redirect('login-page')
+    return redirect("login-page")

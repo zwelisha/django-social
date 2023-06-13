6f7c9ce11f0106a5e7d8ecf80facf59891d3f83a
zwelisha <zwescientist@gmail.com>
2023-06-13 06:38:05 +0200

    added the post model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(max_length=500, default="", null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
